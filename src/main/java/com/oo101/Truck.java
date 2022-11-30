229ab9fd5aa8e8c54096723ad5132cbc09a871f9
Antony Choi <antony.choi@oocl.com>
2022-11-30 16:16:56 +0800

    feat: add truck object

---
+++ src/main/java/com/oo101/Truck.java
@@ -0,0 +1,15 @@
+package com.oo101;
+
+public class Truck {
+    private String name;
+    private int speed;
+
+    public Truck(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public String speedUp(){
+        return String.format("%s: speed up %d km/h", name, speed);
+    }
+}
